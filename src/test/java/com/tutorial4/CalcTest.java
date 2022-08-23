bf6102eaf66b5e5b7b4f1d303f23080bf81a2da4
sqad221 <sqad221@aucklanduni.ac.nz>
2022-08-23 22:45:47 +1200

    a bug! test failed

--- src/test/java/com/tutorial4/CalcTest.java
+++ src/test/java/com/tutorial4/CalcTest.java
@@ -9,4 +9,9 @@ public class CalcTest {
         Calc c = new Calc();
         assertEquals(4, c.add(2,2));
     }
+    @Test
+    void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(0, c.subtract(2,2));
+    }
 }
