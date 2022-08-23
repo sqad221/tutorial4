497038984d88d936430267271b3a183146168672
sqad221 <sqad221@aucklanduni.ac.nz>
2022-08-23 15:46:51 +1200

    tut 4 start

---
+++ src/main/java/com/tutorial4/App.java
@@ -0,0 +1,8 @@
+package com.tutorial4;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/tutorial4/AppTest.java
@@ -0,0 +1,12 @@
+package com.tutorial4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    void assertjShouldWork() {
+        assertThat(true).isTrue();
+    }
+}
